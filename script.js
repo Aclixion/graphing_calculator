4d28b21ec031a7f4809029d25fc6f7872e109d3b
David Lei <DavidtheDefender2003@gmail.com>
2024-03-05 18:31:29 -0800

    Add function to draw vertical grid lines

--- script.js
+++ script.js
@@ -4,3 +4,27 @@ const ctx = canvas.getContext("2d"); // Canvas context
 
 const width = Number(canvas.width); // Width of canvas
 const height = Number(canvas.height); // Height of canvas
+
+// Draws vertical grid lines
+function drawVerticalGridLines(gridWidth) {
+    // Distance from y-axis
+    let x = 0;
+
+    // Draws lines
+    while (x < width) {
+        // Draws line to the right of the y-axis
+        ctx.beginPath();
+        ctx.moveTo(width / 2 + x, 0);
+        ctx.lineTo(width / 2 + x, height);
+        ctx.stroke();
+
+        // Draws line to the left of the y-axis
+        ctx.beginPath();
+        ctx.moveTo(width / 2 - x, 0);
+        ctx.lineTo(width / 2 - x, height);
+        ctx.stroke();
+        
+        // Increases distance between lines and y-axis
+        x += gridWidth;
+    }
+}
