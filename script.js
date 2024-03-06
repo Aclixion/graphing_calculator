23acca34d64e3bdbf058346fa5e442ae754c03f9
David Lei <DavidtheDefender2003@gmail.com>
2024-03-06 00:09:40 -0800

    Add variable for equation input element

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const canvas = document.querySelector("canvas"); // Canvas
 const scaleInput = document.querySelector(".scale-slider"); // Slider for scaling
+const equationInput = document.querySelector(".equation-input"); // Equation input
 const ctx = canvas.getContext("2d"); // Canvas context
 
 const canvasWidth = Number(canvas.width); // Width of canvas
