d34a82447df9d27510f5ab2c5a18cca75781dcb0
David Lei <DavidtheDefender2003@gmail.com>
2024-03-05 16:52:36 -0800

    Add canvas width and height variables

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 const canvas = document.querySelector("canvas"); // Canvas
 const scaleInput = document.querySelector(".scale-slider"); // Slider for scaling
 const ctx = canvas.getContext("2d"); // Canvas context
+
+const width = Number(canvas.width); // Width of canvas
+const height = Number(canvas.height); // Height of cannvas
