7843bcea83fc7448762c6e8230253aea8f03101a
David Lei <DavidtheDefender2003@gmail.com>
2024-03-05 16:52:01 -0800

    Add canvas context

--- script.js
+++ script.js
@@ -1,2 +1,3 @@
 const canvas = document.querySelector("canvas"); // Canvas
 const scaleInput = document.querySelector(".scale-slider"); // Slider for scaling
+const ctx = canvas.getContext("2d"); // Canvas context
